56f1fedbddf7f6e20807bf61d74813e37dc5f913
lagrechef <regis.de.lagrevol92@gmail.com>
2021-05-07 15:31:02 +0200

    test nouvelle brance

---
+++ VELOMAX/Commercial/atelier/Commande.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDD_VELOMAX_APP.VELOMAX.Commercial.atelier
+{
+    class Commande
+    {
+        public int IdCommande { get; private set; }
+
+        public DateTime DateCommande { get; private set; }
+
+        public DateTime DateLivraison { get; private set; }
+
+        public Commande(int IdCommande, DateTime DateCommande, DateTime DateLivraison)
+        {
+            this.IdCommande = IdCommande;
+            this.DateCommande = DateCommande;
+            this.DateLivraison = DateLivraison;
+        }
+        public object ID { get; private set; }
+        public virtual string IdToString() => "IdCommande";
+    }
+}
