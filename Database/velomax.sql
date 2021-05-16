f73cbe83ec3bb4eedf4aa1fb478bf9d375102187
Yanis-sensei <yanis.dahmouche@edu.devinci.fr>
2021-05-16 13:48:15 +0200

    ok

--- Database/velomax.sql
+++ Database/velomax.sql
@@ -298,14 +298,24 @@ insert into Clients(typeClient, nom, idAdresse, telephone, courriel, nomContact)
 values('boutique',"Haribo",1,"06 26 43 43 14","hario@gmail.com","Defer"),
 ('boutique',"EDF",2,"06 46 45 41 24","EDF@gmail.com","Dureau"),
 ('boutique',"PMU",4,"06 36 15 22 18","PMU@gmail.com","Blanc"),
-('boutique',"Michelin",3,"06 56 42 28 08","PMU@gmail.com","Blanc");
+('boutique',"Micheline",3,"06 56 42 28 08","PMU@gmail.com","Blanc");
+ --  (int)reader[val[0]], reader.GetStringSafe(2), reader.GetStringSafe(3), (int)reader[val[4]], reader.GetStringSafe(5), reader.GetStringSafe(6), (int)reader[val[8]], (int)reader[val[10]], reader.GetDateTimeSafe(11)));
 
-  
+insert into Clients(typeClient,prenom,nom,idAdresse,telephone,courriel,nomContact,remise,fidelio,idFidelio,dateAdhesionFidelio)
+values('individuel',"Jean","Raoul",1,"06 26 22 18 40","j.roul@gmail.com","rara",1,2,3,"20/06/13"),
+('individuel',"Louise","Pril",1,"06 46 52 18 41","l.pril@gmail.com",null,1,2,3,"20/06/13"),
+('individuel',"Yanis","Quille",1,"06 16 50 98 44","y.quille@gmail.com",null,1,2,3,"20/06/13"),
+('individuel',"Nabile","zoul",1,"06 06 51 38 34","n.zoul@gmail.com",null,1,2,3,"20/06/13"),
+('individuel',"Marc","desbois",1,"06 52 45 15 23","marcusmorrisfan@gmail.com",null,1,2,3,"20/06/13"),
+('individuel',"Thomas","felin",1,"06 87 54 36 41","thothoaimelamoto@gmail.com",null,1,2,3,"20/06/13"),
+('individuel',"Anas","leroy",1,"06 87 56 52 12","Anasleroi@gmail.com",null,1,2,3,"20/06/13"),
+('individuel',"Delphine","dufresne",1,"06 45 62 35 45","dufresnedel@gmail.com",null,1,2,3,"20/06/13"),
+('individuel',"Valentine","duhamel",1,"06 45 87 51 23","vavadutar@gmail.com",null,1,2,3,"20/06/13"),
+('individuel',"Claude","banta",1,"06 74 83 16 45","bantanavidaloca@gmail.com",null,1,2,3,"20/06/13"),
+('individuel',"Clémence","leveille",1,"06 46 13 56 98","clémence.l@gmail.com",null,1,2,3,"20/06/13"),
+('individuel',"Dominique","gilbert",1,"06 82 19 73 56","domidesdom@gmail.com",null,1,2,3,"20/06/13"),
+('individuel',"Frédérique","lebrun",1,"06 42 51 85 63","frédo.dug@gmail.com",null,1,2,3,"20/06/13"),
+('individuel',"Léon","Quirion",1,"06 42 15 86 32","léonquirion@gmail.com",null,1,2,3,"20/06/13"),
+('individuel',"Christian","Villeneuve",1,"06 12 54 81 67","christistheone@gmail.com",null,1,2,3,"20/06/13");
 
-
-insert into Clients(typeClient, prenom,nom, idAdresse, telephone, courriel,remise,idFidelio)
-value('individuel',"Jean","Roul",5,"06 26 22 18 40","j.roul@gmail.com",4,1),
-('individuel',"Louise","¨Pril",6,"06 46 52 18 41","l.pril@gmail.com",1,2),
-('individuel',"Yanis","Quille",7,"06 16 50 98 44","y.quille@gmail.com",3,3),
-('individuel',"Nabile","zoul",8,"06 06 51 38 34","n.zoul@gmail.com",4,4);
 
