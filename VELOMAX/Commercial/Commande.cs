87605a9e76b6f64ef921a2fa1f837efed9b81c88
Franciskid <Franciskid97@gmail.com>
2021-05-20 19:57:14 +0200

    adz

--- VELOMAX/Commercial/Commande.cs
+++ VELOMAX/Commercial/Commande.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BDD_VELOMAX_APP
 {
-    class Commande: IMySQL
+    public class Commande: IMySQL
     {
         public object ID { get; private set; }
 
---
+++ ViewModels/CommandeViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDD_VELOMAX_APP
+{
+    public class CommandeViewModel
+    {
+        public int IDCommande { get; set; }
+
+        public string Client { get; set; }
+
+        public string PieceCommandée { get; set; }
+
+        public DateTime DateCommande { get; set; }
+
+        public DateTime DateLivraison { get; set; }
+
+
+        public CommandeViewModel() { }
+
+        public CommandeViewModel(Commande com) 
+        {
+            this.IDCommande = com.IDCommande;
+            this.Client = com.Client.AdresseMail;
+            this.PieceCommandée = com.Piece != null ? $"{com.Piece.Nom} : {com.Piece.ID}" : $"{com.Modele.Ligne} : {com.Modele.Nom}";
+            this.DateCommande = com.DateCommande;
+            this.DateLivraison = com.DateLivraison;
+        }
+
+    }
+}
